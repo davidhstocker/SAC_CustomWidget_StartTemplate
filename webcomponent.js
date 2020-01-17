71a2da266e5bc4091542a8226f5b73e4bfc468b8
roadmaptrackerpoc$roadmaptrackerpoc <david.stocker@sap.com>
2020-01-17 10:47:32 +0100

    INitial commit

---
+++ webcomponent.js
@@ -0,0 +1,54 @@
+(function()  {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+      <style>
+      </style>
+    `;
+
+    customElements.define('com-sap-samples-template', class WidgetTemplate extends HTMLElement {
+
+
+		constructor() {
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+		}
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback(){
+        
+        }
+
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback(){
+        
+        }
+
+         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+		onCustomWidgetBeforeUpdate(oChangedProperties) {
+
+		}
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+
+        }
+        
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy(){
+        
+        }
+
+        
+        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+        // Commented out by default
+        /*
+        onCustomWidgetResize(width, height){
+        
+        }
+        */
+    
+    
+    });
+        
+})();
