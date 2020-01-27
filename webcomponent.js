edfd70f1b60b2d862e779c242747af76c4f2bbc2
roadmaptrackerpoc$roadmaptrackerpoc <david.stocker@sap.com>
2020-01-27 15:02:52 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -40,9 +40,13 @@
         }
         
         //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+        /*
         onCustomWidgetResize(width, height){
         
         }
+        */
 
         redraw(
 
